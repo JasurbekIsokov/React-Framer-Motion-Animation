8e4d1ed0ed5dc573a1b64cd5bb7327e7f46f8820
Jasurbek <jasurisoqov685@gmail.com>
2022-05-22 22:32:29 +0500

    navbar link title fixed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ const AnimLink = [
   // Linklar uchun array ichida object
   { link: "Home", to: "#", id: 1 },
   { link: "About", to: "#", id: 2 },
-  { link: "Servisec", to: "#", id: 3 },
+  { link: "Services", to: "#", id: 3 },
   { link: "Blog", to: "#", id: 4 },
   { link: "News", to: "#", id: 5 },
   { link: "Cotact", to: "#", id: 6 },
