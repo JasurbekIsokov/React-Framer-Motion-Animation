166566b5e5d01ea8e7310c692800fe3264d7ca22
Jasurbek <jasurisoqov685@gmail.com>
2022-05-22 20:42:56 +0500

    create navbar and links objet in array

---
+++ src/Components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const AnimLink = [
+  // Linklar uchun array ichida object
+  { link: "Home", to: "#", id: 1 },
+  { link: "About", to: "#", id: 2 },
+  { link: "Servisec", to: "#", id: 3 },
+  { link: "Blog", to: "#", id: 4 },
+  { link: "News", to: "#", id: 5 },
+  { link: "Cotact", to: "#", id: 6 },
+];
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
