705bc7fdeccf5e6537506b9e45d1feeeb17f2b7c
Jasurbek <jasurisoqov685@gmail.com>
2022-05-23 11:45:11 +0500

    dragConstraints

--- src/Components/Drag.js
+++ src/Components/Drag.js
@@ -7,6 +7,10 @@ const Drag = () => {
   return (
     <div className="dragCard">
       <motion.div className="drag-area" ref={ref} />
+      {/*  Manashu div bizga kakbutta devor vazifasini bajaradi . 
+       Shu divdan ichidagi div chiiqib ketmaydi */}
+      <motion.div drag dragConstraints={ref} />
+      {/* dragConstraints cardimizni drag-areadan chiqib ketmasligini taminlaydi */}
     </div>
   );
 };
